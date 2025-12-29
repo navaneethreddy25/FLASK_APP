b272e40b5e035e9bbb4e7941b2d4862f8187f067
navaneethreddy25 <reddynavaneeth555@gmail.com>
2025-12-29 15:45:38 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import requests
+import mysql.connector
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template,request
+from flask_mysqldb import MySQL
 
 load_dotenv()
-
 API_KEY = os.getenv("NINJA_API_KEY")
+con = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_DATABASE")
+    )
+cursor = con.cursor()
 
 if not API_KEY:
     raise ValueError("API key not found. Check your .env file.")
@@ -17,43 +25,47 @@ headers = {
 }
 
 app = Flask(__name__)
-app.secret_key = "secret123"   # required for session
 
 def generate_ai_quote():
     response = requests.get(API_URL, headers=headers)
     data = response.json()
 
     if isinstance(data, list) and len(data) > 0:
-        return data[0]["quote"], data[0]["author"]
-    return "No quote received", "Unknown"
-
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-@app.route("/registration", methods=["GET", "POST"])
-def registration():
-    return render_template("registration.html")
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        session["user"] = request.form.get("username")
-        return redirect(url_for("ai_quotes"))
-    return render_template("login.html")
+        quote = data[0]["quote"]
+        author = data[0]["author"]
+        return quote, author
+    else:
+        return "No quote received", "Unknown"
 
 @app.route("/ai")
-def ai_quotes():
-    if "user" not in session:
-        return redirect(url_for("login"))
-
+def index():
     quote, author = generate_ai_quote()
-    return render_template("index.html", quote=quote, author=author)
+    return render_template(
+        "index.html",
+        quote=quote,
+        author=author
+    )
 
-@app.route("/logout")
-def logout():
-    session.pop("user", None)
-    return redirect(url_for("login"))
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/registration', methods=['GET','POST'])
+def registration():
+    if request.method == "POST":
+        user = request.form["username"]
+        email = request.form['email']
+        password = request.form['password']
+        cursor.execute("insert into registration(username,email,password) values (%s,%s,%s)", [user,email,password])
+        con.commit()
+        return "values stored"
+    return render_template('registration.html') 
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    return render_template('login.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
